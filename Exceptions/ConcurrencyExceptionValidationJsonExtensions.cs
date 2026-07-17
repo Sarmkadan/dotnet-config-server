cbfc5cd8bf3e5487dc224eb171745140905bba5c
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 21:30:00 +0300

    cleanup in ConcurrencyExceptionValidationJsonExtensions

--- Exceptions/ConcurrencyExceptionValidationJsonExtensions.cs
+++ Exceptions/ConcurrencyExceptionValidationJsonExtensions.cs
@@ -3,7 +3,7 @@
 // =============================================================================
 // Author: Vladyslav Zaiets | https://sarmkadan.com
 // CTO & Software Architect
-// =============================================================================
+// =====================================================================
 
 using System;
 using System.Text.Json;
@@ -12,7 +12,7 @@ using System.Text.Json.Serialization;
 namespace DotnetConfigServer.Exceptions;
 
 /// <summary>
-/// Provides System.Text.Json serialization helpers for <see cref="ConcurrencyExceptionValidation"/>.
+/// Provides System.Text.Json serialization helpers for <see cref="ConcurrencyException"/>.
 /// </summary>
 public static class ConcurrencyExceptionValidationJsonExtensions
 {
@@ -25,12 +25,12 @@ public static class ConcurrencyExceptionValidationJsonExtensions
     };
 
     /// <summary>
-    /// Serializes a <see cref="ConcurrencyExceptionValidation"/> instance to a JSON string.
+    /// Serializes a <see cref="ConcurrencyException"/> instance to a JSON string.
     /// </summary>
-    /// <param name="value">The validation instance to serialize.</param>
+    /// <param name="value">The exception instance to serialize.</param>
     /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
-    /// <returns>A JSON string representation of the validation instance.</returns>
-    /// <exception cref="ArgumentNullException">Thrown if <paramref name="value"/> is null.</exception>
+    /// <returns>A JSON string representation of the exception instance.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="value"/> is null.</exception>
     public static string ToJson(this ConcurrencyException? value, bool indented = false)
     {
         ArgumentNullException.ThrowIfNull(value);
@@ -46,10 +46,12 @@ public static class ConcurrencyExceptionValidationJsonExtensions
     }
 
     /// <summary>
-    /// Deserializes a JSON string to a <see cref="ConcurrencyExceptionValidation"/> instance.
+    /// Deserializes a JSON string to a <see cref="ConcurrencyException"/> instance.
     /// </summary>
     /// <param name="json">The JSON string to deserialize.</param>
-    /// <returns>The deserialized <see cref="ConcurrencyExceptionValidation"/> instance, or null if the JSON represents a null value.</returns>
+    /// <returns>The deserialized <see cref="ConcurrencyException"/> instance, or null if the JSON represents a null value.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="json"/> is null.</exception>
+    /// <exception cref="ArgumentException"><paramref name="json"/> is empty or consists only of whitespace.</exception>
     /// <exception cref="JsonException">Thrown if the JSON is invalid or cannot be deserialized.</exception>
     public static ConcurrencyException? FromJson(string json)
     {
@@ -59,11 +61,13 @@ public static class ConcurrencyExceptionValidationJsonExtensions
     }
 
     /// <summary>
-    /// Attempts to deserialize a JSON string to a <see cref="ConcurrencyExceptionValidation"/> instance.
+    /// Attempts to deserialize a JSON string to a <see cref="ConcurrencyException"/> instance.
     /// </summary>
     /// <param name="json">The JSON string to deserialize.</param>
     /// <param name="value">Receives the deserialized instance if successful; otherwise, null.</param>
     /// <returns>True if deserialization succeeded; otherwise, false.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="json"/> is null.</exception>
+    /// <exception cref="ArgumentException"><paramref name="json"/> is empty or consists only of whitespace.</exception>
     public static bool TryFromJson(string json, out ConcurrencyException? value)
     {
         ArgumentException.ThrowIfNullOrEmpty(json);
