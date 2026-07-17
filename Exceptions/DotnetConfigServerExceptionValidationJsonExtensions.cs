16f8b6dc059d66e1cb781efd44f13004ad26f16d
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 21:30:00 +0300

    feat: serialize DotnetConfigServerExceptionValidationJson properly.

---
+++ Exceptions/DotnetConfigServerExceptionValidationJsonExtensions.cs
@@ -0,0 +1,88 @@
+#nullable enable
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
+using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace DotnetConfigServer.Exceptions;
+
+/// <summary>
+/// Provides System.Text.Json serialization and deserialization extension methods for
+/// <see cref="DotnetConfigServerException"/>
+/// </summary>
+public static class DotnetConfigServerExceptionValidationJsonExtensions
+{
+    private static readonly JsonSerializerOptions _jsonSerializerOptions = new(JsonSerializerDefaults.Web)
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        WriteIndented = false,
+        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+        PropertyNameCaseInsensitive = true,
+    };
+
+    /// <summary>
+    /// Serializes a <see cref="DotnetConfigServerException"/> to a JSON string.
+    /// </summary>
+    /// <param name="value">The exception to serialize</param>
+    /// <param name="indented">Whether to format the JSON with indentation for readability</param>
+    /// <returns>A JSON string representation of the exception</returns>
+    /// <exception cref="ArgumentNullException">Thrown when value is null</exception>
+    public static string ToJson(this DotnetConfigServerException value, bool indented = false)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+
+        var options = indented
+            ? new JsonSerializerOptions(_jsonSerializerOptions) { WriteIndented = true }
+            : _jsonSerializerOptions;
+
+        return JsonSerializer.Serialize(value, options);
+    }
+
+    /// <summary>
+    /// Deserializes a JSON string to a <see cref="DotnetConfigServerException"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize</param>
+    /// <returns>The deserialized exception, or null if JSON is empty or whitespace</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null</exception>
+    public static DotnetConfigServerException? FromJson(string json)
+    {
+        ArgumentNullException.ThrowIfNull(json);
+
+        if (string.IsNullOrWhiteSpace(json))
+        {
+            return null;
+        }
+
+        return JsonSerializer.Deserialize<DotnetConfigServerException>(json, _jsonSerializerOptions);
+    }
+
+    /// <summary>
+    /// Attempts to deserialize a JSON string to a <see cref="DotnetConfigServerException"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize</param>
+    /// <param name="value">Receives the deserialized exception if successful</param>
+    /// <returns>True if deserialization succeeded; otherwise false</returns>
+    public static bool TryFromJson(string json, out DotnetConfigServerException? value)
+    {
+        value = null;
+
+        if (string.IsNullOrWhiteSpace(json))
+        {
+            return false;
+        }
+
+        try
+        {
+            value = FromJson(json);
+            return true;
+        }
+        catch (JsonException)
+        {
+            return false;
+        }
+    }
+}
