02d23606cabd2783ef50e328cf8868edd9c3b171
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 21:30:00 +0300

    Tidy up ConcurrencyExceptionExtensions

--- Exceptions/ConcurrencyExceptionExtensions.cs
+++ Exceptions/ConcurrencyExceptionExtensions.cs
@@ -3,11 +3,10 @@
 // =============================================================================
 // Author: Vladyslav Zaiets | https://sarmkadan.com
 // CTO & Software Architect
-// =============================================================================
+// =====================================================================
 
 using System;
 using System.Collections.Generic;
-using System.Globalization;
 
 namespace DotnetConfigServer.Exceptions;
 
@@ -21,132 +20,101 @@ public static class ConcurrencyExceptionExtensions
     /// Determines whether the exception represents an optimistic concurrency conflict.
     /// </summary>
     /// <param name="exception">The concurrency exception to check.</param>
-    /// <returns>True if the exception is an <see cref="OptimisticConcurrencyException"/>; otherwise, false.</returns>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="exception"/> is null.</exception>
+    /// <returns><see langword="true"/> if the exception is an <see cref="OptimisticConcurrencyException"/>; otherwise, <see langword="false"/>.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="exception"/> is <see langword="null"/>.</exception>
     public static bool IsOptimisticConcurrency(this ConcurrencyException exception)
-    {
-        ArgumentNullException.ThrowIfNull(exception);
-        return exception is OptimisticConcurrencyException;
-    }
+    => ArgumentNullException.ThrowIfNull(exception) is OptimisticConcurrencyException;
 
     /// <summary>
     /// Determines whether the exception represents a circular dependency conflict.
     /// </summary>
     /// <param name="exception">The concurrency exception to check.</param>
-    /// <returns>True if the exception is a <see cref="CircularDependencyException"/>; otherwise, false.</returns>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="exception"/> is null.</exception>
+    /// <returns><see langword="true"/> if the exception is a <see cref="CircularDependencyException"/>; otherwise, <see langword="false"/>.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="exception"/> is <see langword="null"/>.</exception>
     public static bool IsCircularDependency(this ConcurrencyException exception)
-    {
-        ArgumentNullException.ThrowIfNull(exception);
-        return exception is CircularDependencyException;
-    }
+        => ArgumentNullException.ThrowIfNull(exception) is CircularDependencyException;
 
     /// <summary>
     /// Extracts the entity type from an optimistic concurrency exception.
-    /// Returns null if the exception is not an optimistic concurrency exception.
+    /// Returns <see langword="null"/> if the exception is not an optimistic concurrency exception.
     /// </summary>
     /// <param name="exception">The concurrency exception.</param>
-    /// <returns>The entity type involved in the conflict, or null.</returns>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="exception"/> is null.</exception>
+    /// <returns>The entity type involved in the conflict, or <see langword="null"/>.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="exception"/> is <see langword="null"/>.</exception>
     public static string? GetEntityType(this ConcurrencyException exception)
     {
         ArgumentNullException.ThrowIfNull(exception);
 
-        if (exception is OptimisticConcurrencyException optException)
+        return exception switch
         {
-            if (optException.Details is IDictionary<string, object> dict)
-            {
-                if (dict.TryGetValue("EntityType", out var entityType))
-                {
-                    return entityType?.ToString();
-                }
-            }
-        }
-
-        return null;
+            OptimisticConcurrencyException optException when optException.Details is IDictionary<string, object> dict
+                => dict.TryGetValue("EntityType", out var entityType) ? entityType?.ToString() : null,
+            _ => null
+        };
     }
 
     /// <summary>
     /// Extracts the entity ID from an optimistic concurrency exception.
-    /// Returns null if the exception is not an optimistic concurrency exception.
+    /// Returns <see langword="null"/> if the exception is not an optimistic concurrency exception.
     /// </summary>
     /// <param name="exception">The concurrency exception.</param>
-    /// <returns>The entity ID involved in the conflict, or null.</returns>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="exception"/> is null.</exception>
+    /// <returns>The entity ID involved in the conflict, or <see langword="null"/>.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="exception"/> is <see langword="null"/>.</exception>
     public static Guid? GetEntityId(this ConcurrencyException exception)
     {
         ArgumentNullException.ThrowIfNull(exception);
 
-        if (exception is OptimisticConcurrencyException optException)
+        return exception switch
         {
-            if (optException.Details is IDictionary<string, object> dict)
-            {
-                if (dict.TryGetValue("EntityId", out var entityId))
-                {
-                    if (entityId is Guid guidId)
-                    {
-                        return guidId;
-                    }
-
-                    if (Guid.TryParse(entityId?.ToString(), out var parsedId))
-                    {
-                        return parsedId;
-                    }
-                }
-            }
-        }
-
-        return null;
+            OptimisticConcurrencyException optException when optException.Details is IDictionary<string, object> dict
+                => dict.TryGetValue("EntityId", out var entityId)
+                    ? entityId switch
+                      {
+                          Guid guidId => guidId,
+                          _ when Guid.TryParse(entityId?.ToString(), out var parsedId) => parsedId,
+                          _ => null
+                      }
+                    : null,
+            _ => null
+        };
     }
 
     /// <summary>
     /// Extracts the expected version from an optimistic concurrency exception.
-    /// Returns null if the exception is not an optimistic concurrency exception.
+    /// Returns <see langword="null"/> if the exception is not an optimistic concurrency exception.
     /// </summary>
     /// <param name="exception">The concurrency exception.</param>
-    /// <returns>The expected version string, or null.</returns>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="exception"/> is null.</exception>
+    /// <returns>The expected version string, or <see langword="null"/>.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="exception"/> is <see langword="null"/>.</exception>
     public static string? GetExpectedVersion(this ConcurrencyException exception)
     {
         ArgumentNullException.ThrowIfNull(exception);
 
-        if (exception is OptimisticConcurrencyException optException)
+        return exception switch
         {
-            if (optException.Details is IDictionary<string, object> dict)
-            {
-                if (dict.TryGetValue("ExpectedVersion", out var expectedVersion))
-                {
-                    return expectedVersion?.ToString();
-                }
-            }
-        }
-
-        return null;
+            OptimisticConcurrencyException optException when optException.Details is IDictionary<string, object> dict
+                => dict.TryGetValue("ExpectedVersion", out var expectedVersion) ? expectedVersion?.ToString() : null,
+            _ => null
+        };
     }
 
     /// <summary>
     /// Extracts the actual version from an optimistic concurrency exception.
-    /// Returns null if the exception is not an optimistic concurrency exception.
+    /// Returns <see langword="null"/> if the exception is not an optimistic concurrency exception.
     /// </summary>
     /// <param name="exception">The concurrency exception.</param>
-    /// <returns>The actual version string, or null.</returns>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="exception"/> is null.</exception>
+    /// <returns>The actual version string, or <see langword="null"/>.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="exception"/> is <see langword="null"/>.</exception>
     public static string? GetActualVersion(this ConcurrencyException exception)
     {
         ArgumentNullException.ThrowIfNull(exception);
 
-        if (exception is OptimisticConcurrencyException optException)
+        return exception switch
         {
-            if (optException.Details is IDictionary<string, object> dict)
-            {
-                if (dict.TryGetValue("ActualVersion", out var actualVersion))
-                {
-                    return actualVersion?.ToString();
-                }
-            }
-        }
-
-        return null;
+            OptimisticConcurrencyException optException when optException.Details is IDictionary<string, object> dict
+                => dict.TryGetValue("ActualVersion", out var actualVersion) ? actualVersion?.ToString() : null,
+            _ => null
+        };
     }
 
     /// <summary>
@@ -155,7 +123,7 @@ public static class ConcurrencyExceptionExtensions
     /// <param name="exception">The concurrency exception.</param>
     /// <param name="retryCount">The number of retry attempts made.</param>
     /// <returns>A formatted message suitable for logging or user display.</returns>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="exception"/> is null.</exception>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="exception"/> is <see langword="null"/>.</exception>
     public static string ToRetryMessage(this ConcurrencyException exception, int retryCount)
     {
         ArgumentNullException.ThrowIfNull(exception);
@@ -179,11 +147,11 @@ public static class ConcurrencyExceptionExtensions
 
     /// <summary>
     /// Determines whether the exception should trigger an automatic retry.
-    /// Returns true for optimistic concurrency and circular dependency exceptions.
+    /// Returns <see langword="true"/> for optimistic concurrency and circular dependency exceptions.
     /// </summary>
     /// <param name="exception">The concurrency exception.</param>
-    /// <returns>True if automatic retry is recommended; otherwise, false.</returns>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="exception"/> is null.</exception>
+    /// <returns><see langword="true"/> if automatic retry is recommended; otherwise, <see langword="false"/>.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="exception"/> is <see langword="null"/>.</exception>
     public static bool ShouldRetryAutomatically(this ConcurrencyException exception)
     {
         ArgumentNullException.ThrowIfNull(exception);
@@ -195,16 +163,14 @@ public static class ConcurrencyExceptionExtensions
     /// </summary>
     /// <param name="exception">The concurrency exception.</param>
     /// <returns>An enumerable of all exception messages in the hierarchy.</returns>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="exception"/> is null.</exception>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="exception"/> is <see langword="null"/>.</exception>
     public static IEnumerable<string> GetAllMessages(this ConcurrencyException exception)
     {
         ArgumentNullException.ThrowIfNull(exception);
 
-        var current = exception;
-        while (current != null)
+        for (var current = exception; current != null; current = current.InnerException as ConcurrencyException)
         {
             yield return current.Message;
-            current = current.InnerException as ConcurrencyException;
         }
     }
 }
