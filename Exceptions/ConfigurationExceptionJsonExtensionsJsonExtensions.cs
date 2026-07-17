3d863915d5886dc15e92c68154c50c2ed195a152
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 21:30:00 +0300

    tighten guard clauses in ConfigurationExceptionJsonExtensionsJsonExtensi

--- Exceptions/ConfigurationExceptionJsonExtensionsJsonExtensions.cs
+++ Exceptions/ConfigurationExceptionJsonExtensionsJsonExtensions.cs
@@ -8,67 +8,70 @@ namespace DotnetConfigServer.Exceptions;
 /// </summary>
 public static class ConfigurationExceptionJsonExtensionsJsonExtensions
 {
-    private static readonly JsonSerializerOptions _jsonOptions = new(JsonSerializerDefaults.Web)
-    {
-        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-        WriteIndented = false,
-        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
-    };
+	private static readonly JsonSerializerOptions _jsonOptions = new(JsonSerializerDefaults.Web)
+	{
+		PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+		WriteIndented = false,
+		DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+	};
 
-    private static readonly JsonSerializerOptions _jsonOptionsIndented = new(JsonSerializerDefaults.Web)
-    {
-        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-        WriteIndented = true,
-        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
-    };
+	private static readonly JsonSerializerOptions _jsonOptionsIndented = new(JsonSerializerDefaults.Web)
+	{
+		PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+		WriteIndented = true,
+		DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+	};
 
-    /// <summary>
-    /// Serializes a <see cref="ConfigurationException"/> instance to a JSON string.
-    /// </summary>
-    /// <param name="value">The exception to serialize.</param>
-    /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
-    /// <returns>A JSON string representation of the exception.</returns>
-    /// <exception cref="ArgumentNullException">Thrown if <paramref name="value"/> is null.</exception>
-    public static string ToJson(this ConfigurationException value, bool indented = false)
-    {
-        ArgumentNullException.ThrowIfNull(value);
+	/// <summary>
+	/// Serializes a <see cref="ConfigurationException"/> instance to a JSON string.
+	/// </summary>
+	/// <param name="value">The exception to serialize.</param>
+	/// <param name="indented">Whether to format the JSON with indentation for readability.</param>
+	/// <returns>A JSON string representation of the exception.</returns>
+	/// <exception cref="ArgumentNullException">Thrown if <paramref name="value"/> is null.</exception>
+	public static string ToJson(this ConfigurationException value, bool indented = false)
+	{
+		ArgumentNullException.ThrowIfNull(value);
 
-        return JsonSerializer.Serialize(value, indented ? _jsonOptionsIndented : _jsonOptions);
-    }
+		return JsonSerializer.Serialize(value, indented ? _jsonOptionsIndented : _jsonOptions);
+	}
 
-    /// <summary>
-    /// Deserializes a JSON string into a <see cref="ConfigurationException"/> instance.
-    /// </summary>
-    /// <param name="json">The JSON string to deserialize.</param>
-    /// <returns>The deserialized <see cref="ConfigurationException"/> instance, or null if the JSON is null or empty.</returns>
-    /// <exception cref="JsonException">Thrown if the JSON is invalid or cannot be deserialized.</exception>
-    public static ConfigurationException? FromJson(string json)
-    {
-        if (string.IsNullOrEmpty(json))
-        {
-            return null;
-        }
+	/// <summary>
+	/// Deserializes a JSON string into a <see cref="ConfigurationException"/> instance.
+	/// </summary>
+	/// <param name="json">The JSON string to deserialize.</param>
+	/// <returns>The deserialized <see cref="ConfigurationException"/> instance, or null if the JSON is null, empty, or whitespace.</returns>
+	/// <exception cref="ArgumentNullException">Thrown if <paramref name="json"/> is null.</exception>
+	/// <exception cref="JsonException">Thrown if the JSON is invalid or cannot be deserialized.</exception>
+	public static ConfigurationException? FromJson(string? json)
+	{
+		ArgumentNullException.ThrowIfNull(json);
 
-        return JsonSerializer.Deserialize<ConfigurationException>(json, _jsonOptions);
-    }
+		return string.IsNullOrWhiteSpace(json)
+			? null
+			: JsonSerializer.Deserialize<ConfigurationException>(json, _jsonOptions);
+	}
 
-    /// <summary>
-    /// Attempts to deserialize a JSON string into a <see cref="ConfigurationException"/> instance.
-    /// </summary>
-    /// <param name="json">The JSON string to deserialize.</param>
-    /// <param name="value">The resulting <see cref="ConfigurationException"/> instance if deserialization succeeds; otherwise, null.</param>
-    /// <returns>True if deserialization succeeds; otherwise, false.</returns>
-    public static bool TryFromJson(string json, out ConfigurationException? value)
-    {
-        try
-        {
-            value = FromJson(json);
-            return true;
-        }
-        catch (JsonException)
-        {
-            value = null;
-            return false;
-        }
-    }
+	/// <summary>
+	/// Attempts to deserialize a JSON string into a <see cref="ConfigurationException"/> instance.
+	/// </summary>
+	/// <param name="json">The JSON string to deserialize.</param>
+	/// <param name="value">The resulting <see cref="ConfigurationException"/> instance if deserialization succeeds; otherwise, null.</param>
+	/// <returns>True if deserialization succeeds; otherwise, false.</returns>
+	/// <exception cref="ArgumentNullException">Thrown if <paramref name="json"/> is null.</exception>
+	public static bool TryFromJson(string? json, out ConfigurationException? value)
+	{
+		ArgumentNullException.ThrowIfNull(json);
+
+		try
+		{
+			value = JsonSerializer.Deserialize<ConfigurationException>(json, _jsonOptions);
+			return true;
+		}
+		catch (JsonException)
+		{
+			value = null;
+			return false;
+		}
+	}
 }
