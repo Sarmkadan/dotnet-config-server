46bd952cec61998908cb84b05432601db82e0771
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 21:30:00 +0300

    Small refactor - DotnetConfigServerExceptionJsonExtensions

--- Exceptions/DotnetConfigServerExceptionJsonExtensions.cs
+++ Exceptions/DotnetConfigServerExceptionJsonExtensions.cs
@@ -2,7 +2,7 @@
 // =============================================================================
 // Author: Vladyslav Zaiets | https://sarmkadan.com
 // CTO & Software Architect
-// =============================================================================
+// =====================================================================
 
 using System.Text.Json;
 using System.Text.Json.Serialization;
@@ -29,15 +29,9 @@ public static class DotnetConfigServerExceptionJsonExtensions
     /// <returns>A JSON string representation of the exception.</returns>
     /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
     public static string ToJson(this DotnetConfigServerException value, bool indented = false)
-    {
-        ArgumentNullException.ThrowIfNull(value);
-
-        var options = indented
+        => JsonSerializer.Serialize(value, indented
             ? new JsonSerializerOptions(_jsonOptions) { WriteIndented = true }
-            : _jsonOptions;
-
-        return JsonSerializer.Serialize(value, options);
-    }
+            : _jsonOptions);
 
     /// <summary>
     /// Deserializes a JSON string to a <see cref="DotnetConfigServerException"/>.
@@ -46,14 +40,9 @@ public static class DotnetConfigServerExceptionJsonExtensions
     /// <returns>The deserialized exception, or null if the JSON is null or empty.</returns>
     /// <exception cref="JsonException">Thrown when the JSON is invalid.</exception>
     public static DotnetConfigServerException? FromJson(string? json)
-    {
-        if (string.IsNullOrEmpty(json))
-        {
-            return null;
-        }
-
-        return JsonSerializer.Deserialize<DotnetConfigServerException>(json, _jsonOptions);
-    }
+        => string.IsNullOrEmpty(json)
+            ? null
+            : JsonSerializer.Deserialize<DotnetConfigServerException>(json, _jsonOptions);
 
     /// <summary>
     /// Attempts to deserialize a JSON string to a <see cref="DotnetConfigServerException"/>.
@@ -63,8 +52,6 @@ public static class DotnetConfigServerExceptionJsonExtensions
     /// <returns>True if deserialization succeeded; otherwise, false.</returns>
     public static bool TryFromJson(string json, out DotnetConfigServerException? value)
     {
-        ArgumentException.ThrowIfNullOrEmpty(json);
-
         try
         {
             value = JsonSerializer.Deserialize<DotnetConfigServerException>(json, _jsonOptions);
