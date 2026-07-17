3c2ae6bbac378648752204f3a55d254da48dfd26
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 21:30:00 +0300

    docs(dotnet-config-server.benchmarks): xml docs on EncryptionBenchmarksJ

--- benchmarks/dotnet-config-server.Benchmarks/EncryptionBenchmarksJsonExtensions.cs
+++ benchmarks/dotnet-config-server.Benchmarks/EncryptionBenchmarksJsonExtensions.cs
@@ -4,6 +4,9 @@ using System.Text.Json.Serialization;
 
 namespace DotnetConfigServer.Benchmarks;
 
+/// <summary>
+/// Provides JSON serialization and deserialization extensions for <see cref="EncryptionBenchmarks"/>.
+/// </summary>
 public static class EncryptionBenchmarksJsonExtensions
 {
     private static readonly JsonSerializerOptions _jsonSerializerOptions = new JsonSerializerOptions
@@ -12,6 +15,12 @@ public static class EncryptionBenchmarksJsonExtensions
         WriteIndented = true
     };
 
+    /// <summary>
+    /// Serializes an <see cref="EncryptionBenchmarks"/> instance to its JSON representation.
+    /// </summary>
+    /// <param name="value">The <see cref="EncryptionBenchmarks"/> instance to serialize.</param>
+    /// <param name="indented">If <c>true</c>, the output JSON will be formatted with indentation; otherwise it will be compact.</param>
+    /// <returns>A JSON string representing the provided <see cref="EncryptionBenchmarks"/> instance.</returns>
     public static string ToJson(this EncryptionBenchmarks value, bool indented = false)
     {
         ArgumentNullException.ThrowIfNull(value);
@@ -25,6 +34,13 @@ public static class EncryptionBenchmarksJsonExtensions
         }
     }
 
+    /// <summary>
+    /// Deserializes a JSON string into an <see cref="EncryptionBenchmarks"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <returns>
+    /// An <see cref="EncryptionBenchmarks"/> instance if deserialization succeeds; otherwise <c>null</c>.
+    /// </returns>
     public static EncryptionBenchmarks? FromJson(string json)
     {
         ArgumentNullException.ThrowIfNull(json);
@@ -38,6 +54,16 @@ public static class EncryptionBenchmarksJsonExtensions
         }
     }
 
+    /// <summary>
+    /// Attempts to deserialize a JSON string into an <see cref="EncryptionBenchmarks"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <param name="value">
+    /// When this method returns, contains the deserialized <see cref="EncryptionBenchmarks"/> instance if the conversion succeeded, or <c>null</c> if it failed.
+    /// </param>
+    /// <returns>
+    /// <c>true</c> if the JSON string was successfully deserialized; otherwise <c>false</c>.
+    /// </returns>
     public static bool TryFromJson(string json, out EncryptionBenchmarks? value)
     {
         ArgumentNullException.ThrowIfNull(json);
