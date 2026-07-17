56dc29db6ec5032105867051b2ad3c2dc1732806
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 21:30:00 +0300

    refactor(events): readability pass over EventBusValidationJsonExtensions

--- Events/EventBusValidationJsonExtensions.cs
+++ Events/EventBusValidationJsonExtensions.cs
@@ -26,20 +26,16 @@ public static class EventBusValidationJsonExtensions
     /// <param name="indented">Whether to format the JSON with indentation.</param>
     /// <returns>A JSON string representation of the <see cref="EventBus"/> instance.</returns>
     /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
-    public static string ToJson(this EventBus value, bool indented = false)
-    {
-        ArgumentNullException.ThrowIfNull(value);
-
-        var options = indented ? _options : new JsonSerializerOptions(_options) { WriteIndented = false };
-        return JsonSerializer.Serialize(value, options);
-    }
+    public static string ToJson(this EventBus value, bool indented = false) =>
+        JsonSerializer.Serialize(value, indented ? _options : new JsonSerializerOptions(_options) { WriteIndented = false });
 
     /// <summary>
     /// Deserializes a JSON string to an <see cref="EventBus"/> instance.
     /// </summary>
     /// <param name="json">The JSON string to deserialize.</param>
     /// <returns>The deserialized <see cref="EventBus"/> instance; or null if deserialization fails.</returns>
-    /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is null or empty.</exception>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null.</exception>
+    /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is empty.</exception>
     public static EventBus? FromJson(string json)
     {
         ArgumentException.ThrowIfNullOrEmpty(json);
@@ -60,7 +56,8 @@ public static class EventBusValidationJsonExtensions
     /// <param name="json">The JSON string to deserialize.</param>
     /// <param name="value">The deserialized <see cref="EventBus"/> instance; or null if deserialization fails.</param>
     /// <returns>True if deserialization is successful; otherwise false.</returns>
-    /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is null or empty.</exception>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null.</exception>
+    /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is empty.</exception>
     public static bool TryFromJson(string json, out EventBus? value)
     {
         ArgumentException.ThrowIfNullOrEmpty(json);
@@ -68,7 +65,7 @@ public static class EventBusValidationJsonExtensions
         try
         {
             value = JsonSerializer.Deserialize<EventBus>(json, _options);
-            return value != null;
+            return value is not null;
         }
         catch (JsonException)
         {
