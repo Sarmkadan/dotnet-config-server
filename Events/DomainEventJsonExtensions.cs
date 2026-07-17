36812bb45a6efdf5cd2441df3623d33991afc1dd
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 21:30:00 +0300

    Dedupe some logic in DomainEventJsonExtensions

--- Events/DomainEventJsonExtensions.cs
+++ Events/DomainEventJsonExtensions.cs
@@ -10,7 +10,7 @@ using System.Text.Json;
 namespace DotnetConfigServer.Events;
 
 /// <summary>
-/// JSON serialization helpers for <see cref="DomainEvent"/> and its derived types.
+/// Provides JSON serialization and deserialization extensions for <see cref="DomainEvent"/> and its derived types.
 /// </summary>
 public static class DomainEventJsonExtensions
 {
@@ -26,11 +26,10 @@ public static class DomainEventJsonExtensions
     /// <param name="value">The <see cref="DomainEvent"/> instance to serialize.</param>
     /// <param name="indented">Whether to format the JSON with indentation.</param>
     /// <returns>A JSON string representation of the <see cref="DomainEvent"/> instance.</returns>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
+    /// <exception cref="ArgumentNullException"><paramref name="value"/> is <see langword="null"/>.</exception>
     public static string ToJson(this DomainEvent value, bool indented = false)
     {
         ArgumentNullException.ThrowIfNull(value);
-
         var options = indented ? _options : new JsonSerializerOptions(_options) { WriteIndented = false };
         return JsonSerializer.Serialize(value, options);
     }
@@ -39,8 +38,8 @@ public static class DomainEventJsonExtensions
     /// Deserializes a JSON string to a <see cref="DomainEvent"/> instance.
     /// </summary>
     /// <param name="json">The JSON string to deserialize.</param>
-    /// <returns>The deserialized <see cref="DomainEvent"/> instance; or null if deserialization fails.</returns>
-    /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is null or empty.</exception>
+    /// <returns>The deserialized <see cref="DomainEvent"/> instance, or <see langword="null"/> if deserialization fails.</returns>
+    /// <exception cref="ArgumentException"><paramref name="json"/> is <see langword="null"/>, empty, or whitespace.</exception>
     public static DomainEvent? FromJson(string json)
     {
         ArgumentException.ThrowIfNullOrEmpty(json);
@@ -59,17 +58,18 @@ public static class DomainEventJsonExtensions
     /// Attempts to deserialize a JSON string to a <see cref="DomainEvent"/> instance.
     /// </summary>
     /// <param name="json">The JSON string to deserialize.</param>
-    /// <param name="value">The deserialized <see cref="DomainEvent"/> instance; or null if deserialization fails.</param>
-    /// <returns>True if deserialization is successful; otherwise false.</returns>
-    /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is null or empty.</exception>
-    public static bool TryFromJson(string json, out DomainEvent? value)
+    /// <param name="value">When this method returns, contains the deserialized <see cref="DomainEvent"/> instance
+    /// if deserialization succeeded; otherwise, <see langword="null"/>.</param>
+    /// <returns><see langword="true"/> if deserialization is successful; otherwise, <see langword="false"/>.</returns>
+    /// <exception cref="ArgumentException"><paramref name="json"/> is <see langword="null"/>, empty, or whitespace.</exception>
+    public static bool TryFromJson(string json, [System.Diagnostics.CodeAnalysis.DisallowNull] out DomainEvent? value)
     {
         ArgumentException.ThrowIfNullOrEmpty(json);
 
         try
         {
             value = JsonSerializer.Deserialize<DomainEvent>(json, _options);
-            return value != null;
+            return value is not null;
         }
         catch (JsonException)
         {
