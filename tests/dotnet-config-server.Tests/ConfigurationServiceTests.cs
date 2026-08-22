b275623fd1be633788da19f1f5dc70b3e65a0980
Vladyslav Zaiets <rutova2@gmail.com>
2026-08-22 21:31:59 +0000

    chore: maintenance updates

--- tests/dotnet-config-server.Tests/ConfigurationServiceTests.cs
+++ tests/dotnet-config-server.Tests/ConfigurationServiceTests.cs
@@ -51,6 +51,8 @@ public class ConfigurationServiceTests
         var config = new Configuration { Id = Guid.NewGuid(), Name = "TestConfig", ApplicationId = Guid.NewGuid() };
         string userId = "user1";
 
+        _loggerMock.Object.LogInformation("Starting test {TestName} with {ConfigName} for user {UserId}", nameof(CreateAsync_ShouldCreateConfiguration_WhenValid), config.Name, userId);
+
         // Act
         _loggerMock.Object.LogInformation("Creating configuration {ConfigId} for user {UserId}", config.Id, userId);
         var result = await _service.CreateAsync(config, userId);
@@ -62,6 +64,8 @@ public class ConfigurationServiceTests
         _configRepositoryMock.Verify(x => x.SaveChangesAsync(), Times.Once);
         _auditLogRepositoryMock.Verify(x => x.AddAsync(It.IsAny<AuditLog>()), Times.Once);
         _auditLogRepositoryMock.Verify(x => x.SaveChangesAsync(), Times.Once);
+
+        _loggerMock.Object.LogInformation("Completed test {TestName} successfully", nameof(CreateAsync_ShouldCreateConfiguration_WhenValid));
     }
 
     /// <summary>
@@ -75,12 +79,16 @@ public class ConfigurationServiceTests
         var config = new Configuration { Id = Guid.NewGuid(), Name = "ChildConfig", ApplicationId = Guid.NewGuid(), ParentConfigurationId = Guid.NewGuid() };
         _configRepositoryMock.Setup(x => x.GetByIdAsync(config.ParentConfigurationId.Value)).ReturnsAsync((Configuration?)null);
 
+        _loggerMock.Object.LogInformation("Starting test {TestName} with {ConfigName} and parent {ParentConfigId}", nameof(CreateAsync_ShouldThrowException_WhenParentNotFound), config.Name, config.ParentConfigurationId);
+
         // Act
         _loggerMock.Object.LogInformation("Creating configuration {ConfigId} with parent {ParentConfigId}", config.Id, config.ParentConfigurationId);
         Func<Task> act = () => _service.CreateAsync(config, "user1");
 
         // Assert
         await act.Should().ThrowAsync<ConfigurationNotFoundException>();
+
+        _loggerMock.Object.LogInformation("Completed test {TestName} successfully", nameof(CreateAsync_ShouldThrowException_WhenParentNotFound));
     }
 
     /// <summary>
@@ -96,6 +104,8 @@ public class ConfigurationServiceTests
         var newConfig = new Configuration { Name = "NewName", ApplicationId = existingConfig.ApplicationId };
         _configRepositoryMock.Setup(x => x.GetByIdAsync(id)).ReturnsAsync(existingConfig);
 
+        _loggerMock.Object.LogInformation("Starting test {TestName} with config id {ConfigId}", nameof(UpdateAsync_ShouldUpdateConfiguration_WhenValid), id);
+
         // Act
         _loggerMock.Object.LogInformation("Updating configuration {ConfigId} for user {UserId}", id, "user1");
         var result = await _service.UpdateAsync(id, newConfig, "user1");
@@ -103,6 +113,8 @@ public class ConfigurationServiceTests
         // Assert
         result.Name.Should().Be("NewName");
         _configRepositoryMock.Verify(x => x.UpdateAsync(It.IsAny<Configuration>()), Times.Once);
+
+        _loggerMock.Object.LogInformation("Completed test {TestName} successfully", nameof(UpdateAsync_ShouldUpdateConfiguration_WhenValid));
     }
 
     /// <summary>
@@ -117,6 +129,8 @@ public class ConfigurationServiceTests
         var config = new Configuration { Id = id, Name = "ConfigToDelete" };
         _configRepositoryMock.Setup(x => x.GetByIdAsync(id)).ReturnsAsync(config);
 
+        _loggerMock.Object.LogInformation("Starting test {TestName} with config id {ConfigId}", nameof(DeleteAsync_ShouldDeleteConfiguration), id);
+
         // Act
         _loggerMock.Object.LogInformation("Deleting configuration {ConfigId} for user {UserId}", id, "user1");
         await _service.DeleteAsync(id, "user1");
@@ -124,6 +138,8 @@ public class ConfigurationServiceTests
         // Assert
         config.DeletedAt.Should().NotBeNull();
         _configRepositoryMock.Verify(x => x.UpdateAsync(config), Times.Once);
+
+        _loggerMock.Object.LogInformation("Completed test {TestName} successfully", nameof(DeleteAsync_ShouldDeleteConfiguration));
     }
 
     /// <summary>
@@ -139,6 +155,8 @@ public class ConfigurationServiceTests
         var key = new ConfigurationKey { Key = "TestKey", Value = "TestValue" };
         _configRepositoryMock.Setup(x => x.GetByIdAsync(configId)).ReturnsAsync(config);
 
+        _loggerMock.Object.LogInformation("Starting test {TestName} with config id {ConfigId} and key {KeyName}", nameof(AddKeyAsync_ShouldAddKey_WhenValid), configId, key.Key);
+
         // Act
         _loggerMock.Object.LogInformation("Adding key {Key} to configuration {ConfigId}", key.Key, configId);
         var result = await _service.AddKeyAsync(configId, key, "user1");
@@ -147,6 +165,8 @@ public class ConfigurationServiceTests
         result.Key.Should().Be("TestKey");
         result.Value.Should().Be("TestValue");
         _keyRepositoryMock.Verify(x => x.AddAsync(key), Times.Once);
+
+        _loggerMock.Object.LogInformation("Completed test {TestName} successfully", nameof(AddKeyAsync_ShouldAddKey_WhenValid));
     }
 
     /// <summary>
@@ -163,11 +183,15 @@ public class ConfigurationServiceTests
         _configRepositoryMock.Setup(x => x.GetByIdAsync(configId)).ReturnsAsync(config);
         _encryptionServiceMock.Setup(x => x.EncryptAsync("PlainText", configId)).ReturnsAsync("EncryptedValue");
 
+        _loggerMock.Object.LogInformation("Starting test {TestName} with config id {ConfigId} and key {KeyName}", nameof(AddKeyAsync_ShouldEncryptKey_WhenConfigEncrypted), configId, key.Key);
+
         // Act
         var result = await _service.AddKeyAsync(configId, key, "user1");
 
         // Assert
         result.IsEncrypted.Should().BeTrue();
         result.Value.Should().Be("EncryptedValue");
+
+        _loggerMock.Object.LogInformation("Completed test {TestName} successfully", nameof(AddKeyAsync_ShouldEncryptKey_WhenConfigEncrypted));
     }
 }
